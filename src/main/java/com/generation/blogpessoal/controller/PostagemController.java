dc70fe19116cde1e1adbbdbc1c98d797164ca6b7
thayrun <132508300+thayrun@users.noreply.github.com>
2023-06-23 12:15:08 -0300

    ATIVIDADE CRUD BACKEND BLOG-PESSOAL – PARTE 01

---
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -0,0 +1,25 @@
+package com.generation.blogpessoal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.generation.blogpessoal.model.Postagem;
+import com.generation.blogpessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostagemController {
+
+	@Autowired
+	private PostagemRepository postagemRepository;
+
+	@GetMapping
+	public ResponseEntity<List<Postagem>> getAll() {
+		return ResponseEntity.ok(postagemRepository.findAll());
+	}
+
+}
