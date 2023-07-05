0ebef42e768367be74b285d9e294b6bb99cb0e17
thayrun <132508300+thayrun@users.noreply.github.com>
2023-07-05 11:27:49 -0300

    Spring Security

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -18,38 +18,61 @@ import jakarta.validation.constraints.Size;
 @Entity
 @Table(name = "tb_postagens")
 public class Postagem {
+    
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long id;
+	
+	@NotBlank(message = "O atributo título é Obrigatório!") 
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo;
+	
+	@NotBlank(message = "O atributo texto é Obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return this.id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getTitulo() {
-		return titulo;
-	}
+    public String getTitulo() {
+        return this.titulo;
+    }
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
-	public String getTexto() {
-		return texto;
-	}
+    public String getTexto() {
+        return this.texto;
+    }
 
-	public void setTexto(String texto) {
-		this.texto = texto;
-	}
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
 
-	public LocalDateTime getData() {
-		return data;
-	}
+    public LocalDateTime getData() {
+        return this.data;
+    }
 
-	public void setData(LocalDateTime data) {
-		this.data = data;
-	}
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
 
 	public Tema getTema() {
 		return tema;
@@ -59,23 +82,12 @@ public class Postagem {
 		this.tema = tema;
 	}
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	public Usuario getUsuario() {
+		return usuario;
+	}
 
-	@NotBlank
-	@Size(min = 3, max = 100)
-	private String titulo;
-
-	@NotBlank
-	@Size(min = 3, max = 1000)
-	private String texto;
-
-	@UpdateTimestamp
-	private LocalDateTime data;
-
-	@ManyToOne
-	@JsonIgnoreProperties("postagem")
-	private Tema tema;
-
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+    
 }
