068384e3841dc98409a20898e5feb6dcb1fcfd28
thayrun <132508300+thayrun@users.noreply.github.com>
2023-07-11 17:41:30 -0300

    Deploy do Projeto Blog Pessoal

--- src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
+++ src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.generation.blogpessoal.configuration;
 
 import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -11,6 +12,7 @@ import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
 
+@Configuration
 public class SwaggerConfig {
 
 	@Bean
@@ -18,43 +20,44 @@ public class SwaggerConfig {
 		return new OpenAPI()
 				.info(new Info()
 						.title("Projeto Blog Pessoal")
-						.description("Projeto Blog Pessoal - Generation Brasil")
+						.description("Projeto Blog Pessoal - Generarion Brasil")
 						.version("v0.0.1")
 						.license(new License()
-								.name("Generation Brasil")
-								.url("https://brazil.generation.org/"))
-								.contact(new Contact()
-								.name("Generation Brasil")		
-								.url("https://github.com/conteudoGeneration")
-								.email("conteudogeneration@generation.org")))
+								.name("Blog Pessoal")
+								.url("https://github.com/frizzly-mika/blogPessoal"))
+						.contact(new Contact()
+								.name("Micael Augusto Dos Santos")
+								.url("https://github.com/frizzly-mika")
+								.email("micael.augustons@hotmail.com")))
 				.externalDocs(new ExternalDocumentation()
-						.description("Github")
-						.url("https://github.com/conteudoGeneration/"));
+						.description("GitHub")
+						.url("https://github.com/conteudoGeneration"));
+	
 	}
 	
 	@Bean
-	OpenApiCustomizer customerGlobalHeaderOpenApiCustomiser() {
-	return openApi -> {
-		openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations()
-				.forEach(operation -> {
-					ApiResponses apiResponses = operation.getResponses();
-					
-					apiResponses.addApiResponse( "200", createApiResponse("Sucesso!"));
-							apiResponses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
-							apiResponses.addApiResponse( "204", createApiResponse("Objeto Excluído!"));
-							apiResponses.addApiResponse( "400", createApiResponse("Erro na Requisição!"));
-							apiResponses.addApiResponse( "401", createApiResponse("Acesso Não Autorizado!"));
-							apiResponses.addApiResponse( "403", createApiResponse("Acesso Proibido"));
-							apiResponses.addApiResponse( "404", createApiResponse("Objeto Não Encontrado!"));
-							apiResponses.addApiResponse( "500", createApiResponse("Erro na Aplicação!"));
-					
-				}));
-	};
-}
-
+	OpenApiCustomizer customerGlobalHeaderOpenApiCustomizer() {
+		
+		return openApi -> {
+			openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations()
+					.forEach(operation -> {
+						
+						ApiResponses apiResponses = operation.getResponses();
+						
+						apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
+						apiResponses.addApiResponse("201", createApiResponse("Objeto persistido!"));
+						apiResponses.addApiResponse("204", createApiResponse("Objeto excluído!"));
+						apiResponses.addApiResponse("400", createApiResponse("Erro na requisição!"));
+						apiResponses.addApiResponse("401", createApiResponse("Acesso não autorizado!"));
+						apiResponses.addApiResponse("403", createApiResponse("Acesso proibido!"));
+						apiResponses.addApiResponse("404", createApiResponse("Objeto não encontrado!"));
+						apiResponses.addApiResponse("500", createApiResponse("Erro na aplicação!"));
+					}));
+		};
+	}
+	
 	private ApiResponse createApiResponse(String message) {
 		
 		return new ApiResponse().description(message);
 	}
-	}
-
+}
--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -14,7 +14,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -25,48 +24,40 @@ public class Usuario {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
-	@NotNull(message = "O Atributo Nome é Obrigatório!")
+	
+	@NotNull(message = "O atributo nome é obrigatório.")
+	@Size(min = 3, max = 255, message = "O atributo nome deve conter no mínimo 03 e no máximo 255 caracteres.")
 	private String nome;
-
+	
 	@Schema(example = "email@email.com.br")
-	@NotNull(message = "O Atributo Usuário é Obrigatório!")
-	@Email(message = "O Atributo Usuário deve ser um email válido!")
+	@NotNull(message = "O atributo usuário é obrigatório.")
+	@Email(message = "O atributo usuário deve ser um email válido.")
 	private String usuario;
-
-	@NotBlank(message = "O Atributo Senha é Obrigatório!")
-	@Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres")
+	
+	@NotNull(message = "O atributo senha é obrigatório.")
+	@Size(min = 8, message = "O atributo senha deve conter no mínimo 08 caracteres.")
 	private String senha;
-
-	@Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
+	
+	@Size(max = 5000, message = "O atributo foto não pode ser maior que 5000 caracteres.")
 	private String foto;
-
+	
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
-	
-	
 
-	public Usuario(Long id, @NotNull(message = "O Atributo Nome é Obrigatório!") String nome,
-			@NotNull(message = "O Atributo Usuário é Obrigatório!") @Email(message = "O Atributo Usuário deve ser um email válido!") String usuario,
-			@NotBlank(message = "O Atributo Senha é Obrigatório!") @Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres") String senha,
-			@Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres") String foto) {
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
 		this.id = id;
 		this.nome = nome;
 		this.usuario = usuario;
 		this.senha = senha;
 		this.foto = foto;
-		
-		
 	}
 
 	public Usuario() {
 	}
 
-	/* Insira os Getters and Setters */
-
 	public Long getId() {
-		return this.id;
+		return id;
 	}
 
 	public void setId(Long id) {
@@ -74,7 +65,7 @@ public class Usuario {
 	}
 
 	public String getNome() {
-		return this.nome;
+		return nome;
 	}
 
 	public void setNome(String nome) {
@@ -82,7 +73,7 @@ public class Usuario {
 	}
 
 	public String getUsuario() {
-		return this.usuario;
+		return usuario;
 	}
 
 	public void setUsuario(String usuario) {
@@ -90,7 +81,7 @@ public class Usuario {
 	}
 
 	public String getSenha() {
-		return this.senha;
+		return senha;
 	}
 
 	public void setSenha(String senha) {
@@ -98,7 +89,7 @@ public class Usuario {
 	}
 
 	public String getFoto() {
-		return this.foto;
+		return foto;
 	}
 
 	public void setFoto(String foto) {
@@ -106,11 +97,12 @@ public class Usuario {
 	}
 
 	public List<Postagem> getPostagem() {
-		return this.postagem;
+		return postagem;
 	}
 
 	public void setPostagem(List<Postagem> postagem) {
 		this.postagem = postagem;
 	}
-
+	
+	
 }
